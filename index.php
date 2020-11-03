afa1f29c724fa83a6404d7a3c3982429b5c1e169
monatalli <41819882+monatalli@users.noreply.github.com>
2020-11-03 12:04:40 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+// echo "hello world";
+
+// echo "<br>";
+
+// echo "mona";
+
+$name="mona tally";
+$age=21;
+
+// echo $name;
+
+// echo "<br>" ;
+
+// echo $age ;
+
+echo "<h1>".$name."<br>".$age ."</h1>";
+echo $name;
+echo $age; 
+
+
+?>
